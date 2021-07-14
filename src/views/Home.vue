41c7727c746a88d02ff086c825eb14d121eb01d7
杨仕淮 <1426339645@qq.com>
2021-07-14 22:27:54 +0800

    Change Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to My PWA App"/>
+  <div>
+    <h1>Welcome my pwa page</h1>
+    <router-link to="/login"></router-link>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 @Component({
   components: {
